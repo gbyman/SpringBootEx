cc1f9d9d15912ea1003d8673c032561217cc618a
gbyman <dongchan-2@hanmail.net>
2020-03-30 23:43:38 +0900

    시큐리티 추가 후 게시물 등록시 한글 깨짐 관련 설정 추가

--- src/main/java/org/zero/config/SecurityConfig.java
+++ src/main/java/org/zero/config/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.zero.security.CustomLoginSuccessHandler;
 import org.zero.security.CustomUserDetailsService;
 
@@ -28,18 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Setter(onMethod_ = {@Autowired})
 	private DataSource dataSource;
-	
-/*	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		log.info("configure...........");
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
-		
-		// $2a$10$PTY/PdIVpI5Jtef.jUyBkeMRnK9M2N.C/TayRarmbvJcmkLmfI4ca
-		auth.inMemoryAuthentication().withUser("member")
-			.password("$2a$10$PTY/PdIVpI5Jtef.jUyBkeMRnK9M2N.C/TayRarmbvJcmkLmfI4ca")
-			.roles("MEMBER");
-	}*/
 	
 	@Bean
 	public AuthenticationSuccessHandler loginSuccessHandler() {
@@ -63,17 +53,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		return repo;
 	}
 	
-	/*@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		log.info("configure...........");
-		String queryUser = "select userid, userpw, enabled from tbl_member where userid = ?";
-		String queryDetails = "select userid, auth from tbl_member_auth where userid = ?";
-		
-		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder())
-			.usersByUsernameQuery(queryUser).authoritiesByUsernameQuery(queryDetails);
-	}*/
-	
 	//in custom userdetails
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -92,11 +71,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// 가장 큰 차이는 xml의 경우 기본으로 POST 방식을 처리하는 경로가 '/login'으로 지정되지만
 		// Java 설정을 이용하는 경우 loginPage()에 해당하는 경로를 기본으로 사용한다는 점
 		// xml과 동일하게 동작하는 것을 목표로 하기 위해 loginProcessingUrl()을 이용해서 '/login'을 지정
-		http.formLogin().loginPage("/customLogin").loginProcessingUrl("/login").successHandler(loginSuccessHandler());
-		
+		//http.formLogin().loginPage("/customLogin").loginProcessingUrl("/login").successHandler(loginSuccessHandler());
+		http.formLogin().loginPage("/customLogin").loginProcessingUrl("/login");
 		http.logout().logoutUrl("/customLogout").invalidateHttpSession(true).deleteCookies("remember-me", "JSESSION_ID");
 		
 		http.rememberMe().key("zero").tokenRepository(persistentTokenRepository()).tokenValiditySeconds(604800);
+		
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		http.addFilterBefore(filter,CsrfFilter.class);
+
 	}
 	
 }
